68603c8fa342b85936f7ba15e8377d49c774430c
frenger88 <64667571+frenger88@users.noreply.github.com>
2020-11-19 19:31:19 -0500

    Testing push

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,14 +1,34 @@
 import React from "react";
 import styled from "styled-components";
+import { FaShoppingCart as CartIcon } from "react-icons/fa";
 
 const Header = () => {
-  return <Wrapper></Wrapper>;
+  return (
+    <Wrapper>
+      <h1>Fetch Store</h1>
+      <Navigation>
+        Link Link Link Link
+        <input />
+        <CartIcon size={55} />
+      </Navigation>
+      <h1>cart</h1>
+    </Wrapper>
+  );
 };
 
 export default Header;
 
 const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  box-sizing: border-box;
+  padding: 10px;
   height: 5%;
   min-height: 60px;
   background-color: pink;
+`;
+
+const Navigation = styled.div`
+  width: 60%;
 `;
