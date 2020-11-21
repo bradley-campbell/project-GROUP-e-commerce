4fe7961314c7241082e92f42bfc8aa4798c1597a
pinghsuan chueh <pinghsuan.chueh@mail.mcgill.ca>
2020-11-20 22:24:07 -0500

    i) added company/:companyId endpoint (missed it before), ii) deubgged conversion to id function

--- client/src/components/ProductByCompany.js
+++ client/src/components/ProductByCompany.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
+import Grid from "./Grid";
 
 // Fetch items by company selected
 const ProductByCompany = () => {
   const { companyId } = useParams();
   const [cProducts, setCProducts] = useState(null);
+  const [comInfo, setComInfo] = useState(null);
 
   useEffect(() => {
     fetch(`/product/by-company/${companyId}`)
@@ -13,13 +15,24 @@ const ProductByCompany = () => {
       .then((info) => {
         if (info.status === 200) {
           setCProducts(info.products);
+          setComInfo(info.company);
         } else {
           console.log(info.error); // may direct to error page
         }
       });
   }, []);
 
-  return cProducts ? <div></div> : <div>Loading page</div>;
+  return cProducts ? (
+    <PCWrapper>
+      <PCTitle>Products from company {cProducts[0].companyId}</PCTitle>
+      <Grid itemsData={cProducts} />
+    </PCWrapper>
+  ) : (
+    <div>Loading page</div>
+  );
 };
 
 export default ProductByCompany;
+
+const PCWrapper = styled.div``;
+const PCTitle = styled.div``;
