febb73e0054364d032a3a03c2af96eb07b502519
pinghsuan chueh <pinghsuan.chueh@mail.mcgill.ca>
2020-11-21 16:15:22 -0500

    debugged

--- client/src/components/CompanyList.js
+++ client/src/components/CompanyList.js
@@ -1,14 +1,68 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 
 // Fetch items by company selected
 
 const CompanyList = () => {
+  const [comps, setComps] = useState(null);
+
+  useEffect(() => {
+    fetch(`/company/all`)
+      .then((data) => data.json())
+      .then((info) => {
+        if (info.status === 200) {
+          console.log(info);
+          setComps(info.companies);
+        } else {
+          console.log(info.error); // may direct to error page
+        }
+      });
+  }, []);
+
   return (
-    <div>
-      <h1>COMPANY LIST</h1>
-    </div>
+    comps && (
+      <CWrapper>
+        <h1>COMPANY LIST</h1>
+        <CompanyGrid companies={comps} />
+      </CWrapper>
+    )
   );
 };
 
 export default CompanyList;
+const CWrapper = styled.div`
+  margin-top: 30px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+// ================ Company Wrappers ================
+const CompanyGrid = ({ companies }) => {
+  return (
+    <CGWrapper>
+      {companies.map((ele) => (
+        <CompanyItem key={`${ele.id}`} company={ele} />
+      ))}
+    </CGWrapper>
+  );
+};
+
+const CGWrapper = styled.div`
+  width: 100vw;
+  display: flex;
+  height: auto;
+`;
+
+// ================ Company items ================
+const CompanyItem = ({ company }) => {
+  const { name, id, url, country } = company;
+
+  return <CIWrapper>{name}</CIWrapper>;
+};
+
+const CIWrapper = styled.div`
+  width: 200px;
+  height: 100px;
+  border: 1px solid red;
+`;
--- client/src/components/ProductByCompany.js
+++ client/src/components/ProductByCompany.js
@@ -22,9 +22,9 @@ const ProductByCompany = () => {
       });
   }, []);
 
-  return cProducts ? (
+  return cProducts && comInfo ? (
     <PCWrapper>
-      <PCTitle>Products from company {cProducts[0].companyId}</PCTitle>
+      <PCTitle>Products from company {comInfo.name} </PCTitle>
       <Grid itemsData={cProducts} />
     </PCWrapper>
   ) : (
@@ -34,5 +34,12 @@ const ProductByCompany = () => {
 
 export default ProductByCompany;
 
-const PCWrapper = styled.div``;
-const PCTitle = styled.div``;
+const PCWrapper = styled.div`
+  margin-top: 30px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+const PCTitle = styled.h1`
+  color: black; 
+`;
