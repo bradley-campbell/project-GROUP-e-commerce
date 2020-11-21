6bbd4c6dfc0b27bbc1943d5a066c004d4cf6a3e4
lenakroeker <lenakroeker@gmail.com>
2020-11-21 01:02:31 -0500

    styled items header layout companies page

--- client/src/GlobalStyles.js
+++ client/src/GlobalStyles.js
@@ -1,35 +1,18 @@
 import React from "react";
 import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
 
 const GlobalStyle = createGlobalStyle`
 
+${reset};
 
-:root{
-
-    --navColor: #011627;  // Rich Black
-    --mainBackground:  #EDC79B; // Gold Crayola
-    --accent:  #AA6373; // Rose Dust
-    --productBackground: #9FC490; // Dark Sea Green
-    --mainText : #AEC5EB
-
-}
-
-*{
+body{
     font-family: 'Inter', sans-serif;
 }
 
-h1, h2 ,h3, p{
-    color: var(--mainText);
+a{
+    text-decoration: none;
 }
-
-nav{
-    background-color: var(--navColor);
-}
-
-body{
-    background-color: var(--mainBackground)
-}
-
 
 `;
 
--- client/src/components/CompanyList.js
+++ client/src/components/CompanyList.js
@@ -1,14 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
+import { COLORS } from "../ConstantStyles";
+import { Link } from "react-router-dom";
 
 // Fetch items by company selected
 
 const CompanyList = () => {
-  return (
-    <div>
-      <h1>COMPANY LIST</h1>
-    </div>
+  const [companies, setCompanies] = useState("");
+
+  useEffect(() => {
+    fetch(`/company/all`)
+      .then((res) => res.json())
+      .then((res) => {
+        setCompanies(res.companies);
+      });
+  }, []);
+  console.log(companies);
+
+  return companies ? (
+    <Wrapper>
+      {companies.map((company) => {
+        return (
+          <CompanyBox exact to="/company/companyid">
+            <Name>{company.name}</Name>
+          </CompanyBox>
+        );
+      })}
+    </Wrapper>
+  ) : (
+    <div>loading</div>
   );
 };
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
+const CompanyBox = styled(Link)`
+  padding: 10px;
+  border: none;
+  width: 200px;
+  height: 60px;
+  color: ${COLORS.primary};
+  text-align: center;
+  background: ${COLORS.secondary};
+  border-radius: ${COLORS.borderRadius};
+  margin: 20px;
+`;
+
+const Name = styled.h2`
+  font-size: 20px;
+`;
 
 export default CompanyList;
