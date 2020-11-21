c73275ae44b8742dc22e3febc2e0ea77f3b0e0b1
frenger88 <64667571+frenger88@users.noreply.github.com>
2020-11-21 18:51:43 -0500

    Created basic modal component to be toggled using state - needs styling

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { AiOutlineCloseCircle } from "react-icons/ai";
+import Cart from "./Cart";
 
 const Modal = ({ closeModal }) => {
   return (
@@ -8,7 +9,9 @@ const Modal = ({ closeModal }) => {
       <button onClick={closeModal}>
         <AiOutlineCloseCircle />
       </button>
-      <Content></Content>
+      <Content>
+        <Cart />
+      </Content>
     </Overlay>
   );
 };
@@ -16,27 +19,27 @@ const Modal = ({ closeModal }) => {
 export default Modal;
 
 const Overlay = styled.div`
-position: fixed;
-display: block;
-overflow: auto;
-width: 100%;
-height: 100%;
-top: 0;
-left: 0;
-right: 0;
-bottom: 0;
-background-color: rgba(0,0,0,0.5);
-z-index: 999;
-cursor: pointer;
+  position: fixed;
+  display: block;
+  overflow: auto;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 999;
+  cursor: pointer;
 `;
 
 const Content = styled.div`
-margin: 15% auto;
-background-color: white;
-
-
-
-
+  margin: 15% auto;
+  background-color: white;
+  border-radius: 0.25 rem;
+  width: 50vw;
+  padding: 2rem;
+  position: relative;
 `;
 
 const ExitButton = styled.button`
