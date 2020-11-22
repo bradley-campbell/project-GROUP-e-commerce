e91f28e71182a80ceccc3cfe663aaa494c3e9cee
frenger88 <64667571+frenger88@users.noreply.github.com>
2020-11-21 20:43:23 -0500

    Working on payment form

--- client/src/components/Payment.js
+++ client/src/components/Payment.js
@@ -1,12 +1,45 @@
 import React from "react";
 import styled from "styled-components";
+import { AiOutlineCloseCircle } from "react-icons/ai";
 
-const Payment = () => {
+const Payment = ({ closeModal }) => {
   return (
-    <div>
-      <h1>Payment</h1>;
-    </div>
+    <Overlay>
+      <button onClick={closeModal}>
+        <AiOutlineCloseCircle />
+      </button>
+      <Content></Content>
+    </Overlay>
   );
 };
 
 export default Payment;
+
+const Overlay = styled.div`
+  position: fixed;
+  display: block;
+  overflow: auto;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 999;
+  cursor: pointer;
+`;
+
+const Content = styled.div`
+  margin: 15% auto;
+  background-color: white;
+`;
+
+const ExitButton = styled.button`
+  padding: 10px;
+  cursor: pointer;
+  border: 0;
+  position: absolute;
+  top: 0.3rem;
+  right: 0.5 rem;
+`;
