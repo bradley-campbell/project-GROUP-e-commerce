607a01d763c20368fd5bcda26a15b18862ac67aa
frenger88 <64667571+frenger88@users.noreply.github.com>
2020-11-24 07:43:28 -0500

    Saving progress

--- client/src/reducers/test.js
+++
@@ -1,7 +0,0 @@
-// import { combineReducers } from "redux";
-
-// import cart, { getStoreItemsArray } from "./cart-reducer";
-// import validation from "./validation-reducer";
-
-// export default combineReducers({ cart, validation });
- 
