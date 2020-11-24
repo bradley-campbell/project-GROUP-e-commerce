275e66b3a967be3df08e56ce6a6ac5bb450e2b70
lenakroeker <lenakroeker@gmail.com>
2020-11-24 18:30:25 -0500

    fix fetch num items

--- client/src/components/All.js
+++ client/src/components/All.js
@@ -6,7 +6,7 @@ export const All = () => {
   const [items, setItems] = useState("");
 
   useEffect(() => {
-    fetch(`/product/random/400`)
+    fetch(`/product/random/349`)
       .then((res) => res.json())
       .then((res) => {
         setItems([...res.products]);
